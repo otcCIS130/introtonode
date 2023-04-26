bfe07a39e4e848a2f4fed293b7ecda8a0cee4144
AGUILAR <oa0976959@dt038574.otc.edu>
2023-04-26 12:53:24 -0500

    added axios

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import chalk from 'chalk';
+import axios from 'axios';
 
 console.log(chalk.blue('Hello world!'));
+import scripts from './scripts.js';
+import { configs } from './scripts.js';
+scripts();
+configs();
+
+axios.get("https://dog.ceo/api/breeds/image/random")
+.then (res => {
+    console.log(res.data)
+})
---
+++ scripts.js
@@ -0,0 +1,7 @@
+export default function(){
+    console.log("I am a default function")
+}
+
+export function configs(){
+    console.log("I am a config function")
+}
